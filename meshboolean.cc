c28126223d14e629ccd8fcdb089212d21c3f15db
Iyad Ahmed <iyadahmed430@gmail.com>
2022-09-03 21:09:03 +0200

    Rename meshboolean main file

--- meshboolean.cc
+++ meshboolean.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 
 #include "ball_tree.hh"
@@ -20,10 +21,10 @@ int main(int argc, char **argv) {
   auto tris = read_binary_stl_unchecked(filename);
   std::cout << "Number of triangles = " << tris.size() << std::endl;
 
-  std::vector<BVH::Triangle> bvh_tris;
-  for (const auto &t : tris) {
-    bvh_tris.push_back({t.verts[0], t.verts[1], t.verts[2]});
-  }
+  //  std::vector<BVH::Triangle> bvh_tris;
+  //  for (const auto &t : tris) {
+  //    bvh_tris.push_back({t.verts[0], t.verts[1], t.verts[2]});
+  //  }
 
   std::vector<Vec3> vertices;
   for (const auto &t : tris) {
@@ -35,9 +36,14 @@ int main(int argc, char **argv) {
   Timer timer;
   Ball_Tree2 ball_tree(vertices);
   timer.tock("Constructing Ball Tree");
-  std::cout << "Number of Ball Tree leaf nodes = " << ball_tree.count_leaf_nodes(0)
-            << std::endl;
+  std::cout << "Number of Ball Tree leaf nodes = "
+            << ball_tree.count_leaf_nodes(0) << std::endl;
   tassert(ball_tree.count_leaf_nodes(0) == vertices.size());
+
+  std::queue<Vec3> Q;
+  ball_tree.knn_search(Q, vertices[0], 1);
+  std::cout << vertices[0] << std::endl;
+  std::cout << Q.front() << std::endl;
 
   //  for (const auto &v : vertices) {
   //  if (not ball_tree.has_close_point(vertices[0], .001)) {
@@ -45,11 +51,11 @@ int main(int argc, char **argv) {
   //  }
   //  }
 
-  timer.tick();
-  BVH bvh(bvh_tris);
-  timer.tock("Building BVH");
-  std::cout << "Number of BVH nodes = " << bvh.calc_number_of_nodes()
-            << std::endl;
+  //  timer.tick();
+  //  BVH bvh(bvh_tris);
+  //  timer.tock("Building BVH");
+  //  std::cout << "Number of BVH nodes = " << bvh.calc_number_of_nodes()
+  //            << std::endl;
 
   return 0;
 }
