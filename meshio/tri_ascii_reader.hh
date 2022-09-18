476cec88b070527a8c93515090737ab0e6f237d3
Iyad Ahmed <iyadahmed430@gmail.com>
2022-09-18 21:09:20 +0200

    Refactor .tri mesh file reader

--- meshio/tri_ascii_reader.hh
+++ meshio/tri_ascii_reader.hh
@@ -15,10 +15,21 @@ using Triangle = std::array<Vec3, 3>;
 class Tri_Mesh_ASCII_file_reader : NonCopyable {
 private:
   static constexpr int MAX_LINE_LENGTH = 100;
-
   FILE *file;
-  uint8_t header[80]{};
-  uint32_t number_of_triangles = 0;
+
+  static float read_float(char **string) {
+    errno = 0;
+    char *end = *string;
+    float res = strtof(*string, &end);
+    if (*string == end) {
+      throw std::runtime_error("Failed to parse float value");
+    }
+    if (errno == ERANGE) {
+      throw std::out_of_range("Parsed float value is out of range");
+    }
+    *string = end;
+    return res;
+  }
 
 public:
   explicit Tri_Mesh_ASCII_file_reader(const char *filename) {
@@ -40,25 +51,12 @@ public:
       }
     }
     Triangle t;
-    //    if (fscanf(file, "%f %f %f %f %f %f %f %f %f\n", &t[0][0], &t[0][1],
-    //               &t[0][2], &t[1][0], &t[1][1], &t[1][2], &t[2][0], &t[2][1],
-    //               &t[2][2]) != 9) {
-    //      return std::nullopt;
-    //    }
     line[MAX_LINE_LENGTH] = '\0';
-    char *c = line;
-    char *end = c;
-    float *values = reinterpret_cast<float *>(&t[0][0]);
-    for (int i = 0; i < 9; i++) {
-      errno = 0;
-      values[i] = strtof(c, &end);
-      if (c == end) {
-        throw std::runtime_error("Failed to parse float value");
+    char *str = line;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        t[i][j] = read_float(&str);
       }
-      if (errno == ERANGE) {
-        throw std::out_of_range("Parsed float value is out of range");
-      }
-      c = end;
     }
     return t;
   }
